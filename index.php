e0eca80fa62731e1403868379b5013a29f6b7d91
麦当苗儿 <zuojiazi@topthink.net>
2015-08-11 16:21:58 +0800

    更改公共目录接口及入口常量定义

--- index.php
+++ index.php
@@ -9,25 +9,53 @@
 // | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <www.zjzit.cn>
 // +----------------------------------------------------------------------
 
-// PHP环境检测
-// ThinkPHP要求5.3.0以上的PHP版本
-version_compare(PHP_VERSION, '5.3.0', '<') && exit('require PHP > 5.3.0 !');
+/**
+ * PHP环境检测
+ * ThinkPHP3.2.3要求PHP5.3.0以上版本
+ * 但是OneCMF要求PHP5.4.0以上版本
+ */
+version_compare(PHP_VERSION, '5.4.0', '<') && exit('require PHP > 5.3.0 !');
 
-// 调试模式 
-// 建议开发阶段开启 部署阶关闭
+/**
+ * 应用调试模式 
+ * 建议开发阶段开启 部署阶关闭
+ */
 define('APP_DEBUG', true);
 
-// OneCMF缓存目录
+/**
+ * OneCMF缓存目录
+ * OneCMF所有的模板缓存，文件形式的数据缓存，零时文件
+ * 以及日志文件都将保存在该目录
+ * 该目录可以部署到Web以外的目录，但部署完成后请更改路径配置
+ * 注意: 该目录必须可写，尽量使用绝对路径
+ */
 define('RUNTIME_PATH', realpath('./Runtime') . '/');
 
-// OneCMF核心目录
+/**
+ * OneCMF核心目录
+ * OneCMF的工作目录，所有OneCMF的API，模块，工具类库等目录都定义在该目录
+ * 该目录也是OC命名空间的更目录
+ * 所有除开ThinkPHP模块的类库都必须使用OC命名空间
+ */
 define('ONECMF_PATH', realpath('./OneCMF') . '/');
 
-// 应用目录
+/**
+ * ThinkPHP公共模块目录
+ * 该目录为ThinkPHP必须目录，但是在OneCMF中已经被ONECMF_PATH代替
+ * 因此，该常量为了兼容而定义
+ */
+define('COMMON_PATH', ONECMF_PATH);
+
+/**
+ * OneCMF应用目录（模块目录）
+ * 即ThinkPHP的模块目录，每个目录代表一个模块
+ * 所有OneCMF的核心块，扩展模块都将定义在该目录
+ * 该目录不受OC命名空间影响，直接遵循ThinkPHP模块命名空间即可
+ */
 define('APP_PATH', ONECMF_PATH . 'Application/');
 
-// 应用公共目录
-define('COMMON_PATH', ONECMF_PATH . 'Common/');
-
-// 执行应用
+/**
+ * 执行应用
+ * OneCMF应用执行入口（即ThinkPHP入口）
+ */
 require './ThinkPHP/ThinkPHP.php';
